b21215a53d8ba02e41b68159611f2333f17eb4fc
Fred Perrier <frederique.perrier@gmail.com>
2022-01-04 16:41:22 +0100

    Refonte validation formulaire inscription (rendu du P5), enregistrement nouvel utilisateur BDD OK, malgré prbls terminal VSC

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -13,47 +13,30 @@ exports.allUsers = async function allUser(req, res, next){
 // Enregistrement d'un utilisateur avec hash  
 
 exports.signup = async function signup(req, res, next){
-    try
-    {
-        const user = await prisma.user.findUnique({
-            where: { 
-                email: req.body.email 
-            }
-        })
-        console.log('user:', user);
-
-        if(user)
-        {
-            res.send(JSON.stringify({'status': 302, "error": 'Cet email existe déjà dans la base de données'}));
-            return;
-        } else {
-            let passwordHash = bcrypt.hash(req.body.password, 10);
-            try 
-            {
-                console.log('password:', passwordHash);
-                const user = await prisma.user.create({
-                    data: {
-                        first_name: req.body.firstName,
-                        last_name: req.body.lastName,
-                        email: req.body.email,
-                        password: passwordHash
-                    }
-                })
-                res.send(JSON.stringify({'status': 200, 'error': null, 'response': user.id}));
-            }
-            catch(e)
-            {
-                res.send(JSON.stringify({'status': 500, 'error': 'In create user '+e, 'response': null}));
-            }
+    const userExists = await prisma.user.findUnique({
+        where: {
+            email: String(req.body.email)
+        }, select: {
+            email: true
         }
-        
+    })
+    if(userExists){
+        return res.status(400).json({ message : 'Cet email existe déjà dans la base de données'})
     }
-    catch(e)
-        {
-            res.send(JSON.stringify({'status': 501, 'error': 'In user '+e, 'response': null}));
-        }
-};
 
+    const hash = await bcrypt.hash(req.body.password, 10);
+
+    const newUser = await prisma.user.create({
+        data: {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: hash
+        },
+    })
+    res.json(newUser)
+    .catch (error, res.status(401).json({ error: error | 'Requête non authentifiée'})) 
+};
 
 // Connection d'un utilisateur 
 
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,145 +1,162 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "./Header";
 import Login from "./Login";
 import '../style/signup.css';
 
-const Signup= () =>{
-    const initialsValues = { firstName: '', lastName: '', email: '', password: '', verifPassword: ''};
-    const [formValues, setFormValues] = useState(initialsValues);
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
 
-    const handleChange = (e) => {
-
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value})
-    };
+const Signup = () => {
+    const [formSumbit, setFormSubmit] = useState(false);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [checkPassword, setCheckPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setFormErrors(validate(formValues));
-        setIsSubmit(true);
-    }
+        // Fonction de vérification d'un champ du fomulaire avec un regex
+        const isValidInput = function (inputField, inputValue, regex, invalidMessage) {
+            const small = inputField.nextElementSibling;
 
-    useEffect(() => {
-        if (Object.keys(formErrors).length === 0 && isSubmit) {
-        console.log(formValues);
-        }
-    }, [formErrors, formValues, isSubmit]);
-
-    const validate = (values) => {
-        const errors = {};
-
-        const firstNameRegex = new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ-\' ]+$', 'g');
-        const lastNameRegex = new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ-\' ]+$', 'g');
-        const emailRegex = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,5}$', 'g');
-        const passwordRegex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$');
-
-        if(!values.firstName){
-            errors.firstName = '❌ Le prénom est obligatoire'
-        } else if (!firstNameRegex.test(values.firstName.trim())){
-            errors.firstName = '❌ Le prénom ne doit comporter que des lettres (espaces, tirets et apostrophes autorisés)'
-        }
-        if(!values.lastName){
-            errors.lastName = '❌ Le nom est obligatoire'
-        } else if (!lastNameRegex.test(values.lastName.trim())){
-            errors.lastName = '❌ Le nom ne doit comporter que des lettres (espaces, tirets et apostrophes autorisés)'
-        }
-        if(!values.email){
-            errors.email = '❌ L\'adresse mail est obligatoire'
-        } else if (!emailRegex.test(values.email.trim())){
-            errors.email = '❌ Veuillez entrer une adresse mail valide'
-        }
-        if(!values.password){
-            errors.password= '❌ Le mot de passe est obligatoire'
-        } else if (!passwordRegex.test(values.password.trim())){
-            errors.password = '❌ Le mot de passe doit comporter au minimum 8 caractères dont une majucule, une minuscule, un chiffre et un caractère spécial'
-        }
-        if(values.password !== values.verifPassword){
-            errors.verifPassword = '❌ Mots de passe différents, réessayez.'
-        }
-        return errors;    
-    }
-    
-    if(validate && !setFormErrors){
-        let userData = {
-            first_name: formValues.firstName,
-            last_name: formValues.lastName,
-            email: formValues.email,
-            password: formValues.password
-        }
-
-        let fetchData = {
-            method: 'POST',
-            body: JSON.stringify(userData),
-            headers: {
-                'Content-Type': 'application/json'
+            if (regex.test(inputValue)) {
+                small.innerHTML = '✅ OK';
+                small.classList.remove('text-danger');
+                small.classList.add('text-success');
+                inputField.style.borderColor = 'green';
+                return true;
+            } else if (!regex.test(inputValue)) {
+                small.innerHTML = invalidMessage;
+                small.classList.remove('text-success');
+                small.classList.add('text-danger');
+                inputField.style.borderColor = 'red';
+                return false;
             }
-        }
+        };
 
-
-        fetch('http://localhost:5500/api/auth/signup', fetchData)
-            .then(response => response.json())
-            .then(() => {
-                localStorage.setItem('user', JSON.stringify(userData));
-                setIsSubmit(true)
-            })
-            //.catch(error => res.status(500).json({ error : 'Problème lors de l\'enregistrement, veuillez recommencer votre inscription.' }))
+        // Création de l'input , du regex et du message d'erreur pour chaque champs du formulaire
         
-    };
+        const firstNameRegex = new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ-\' ]+$', 'g');
+        const firstNameInput = document.getElementById('firstName');
+        const firstNameInvalid = '⛔ Le prénom ne doit comporter que des lettres (espace, tiret et apostrophe autorisés)';
 
-    return(
-        <>
-            {Object.keys(formErrors).length === 0 && isSubmit ? (
-                <>
-                    <div>Enregistrement réussi, vous pouvez maintenant vous connecter au réseau social</div>
-                    <Login />
-                </>
-            ) : (
-                <div>
-                <Header/>
-                <div className="d-flex justify-content-center">
-                    <div className="card p-3 m-3 col-md-8 d-flex align-items-center">
-                        <div className="mt-3 mb-4 h4">Inscription au réseau social de Groupomania</div>
-                        <form action='' onSubmit={ handleSubmit } method='POST' className="col-md-10 col-lg-7">
-                            <div className="form-group">
-                                <label htmlFor='firstName' className="form-label">Prénom : </label>
-                                <input type='text' name='firstName' id='firstName' className="form-control" onChange={ handleChange} value= { formValues.firstName } /> 
-                                <small className="small ">{formErrors.firstName}</small>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor='lastName' className="form-label">Nom : </label>
-                                <input type='text' name='lastName' id='lastName' className="form-control" onChange={ handleChange} value= { formValues.lastName }  />
-                                <small className="small">{formErrors.lastName}</small> 
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor='email' className="form-label">Adresse e-mail : </label>
-                                <input type='text' name='email' id='email' className="form-control" onChange={ handleChange} value= { formValues.email } /> 
-                                <small className="small">{formErrors.email}</small>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor='password' className="form-label"> Mot de passe : </label>
-                                <input type="password" name='password' id='password' className="form-control" onChange={ handleChange} value= { formValues.password } /> 
-                                <small className="small">{formErrors.password}</small>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor='verifPassword' className="form-label"> Vérification du mot de passe : </label>
-                                <input type="password" name='verifPassword' id='verifPassword' className="form-control" onChange={ handleChange} value= { formValues.verifPassword } /> 
-                                <small className="small">{formErrors.verifPassword}</small>
-                            </div>
-                            <div >
-                                <button type='submit' className="btn mt-3 rounded border">Inscription </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+        const lastNameRegex = new RegExp("^([A-Za-z]+)[' -]?([A-Za-z]+)$", "g");
+        const lastNameInput = document.getElementById('lastName');
+        const lastNameInvalid = '⛔ Le nom ne doit comporter que des lettres (espace, tiret et apostrophe autorisés)';
+
+        const emailRegex = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,5}$', 'g');
+        const emailInput = document.getElementById('email');
+        const emailInvalid = '⛔ Veuillez entrer une adresse mail valide';
+
+        const passwordRegex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$', 'g')
+        const passwordInput = document.getElementById('password');
+        const passwordInvalid= '⛔ Le mot de passe doit comporter au minimum 8 caractères dont une majucule, une minuscule, un chiffre et un caractère spécial';
+
+ 
+        let validFirstName = isValidInput(firstNameInput, firstName, firstNameRegex, firstNameInvalid);
+        let validLastName = isValidInput(lastNameInput, lastName, lastNameRegex, lastNameInvalid);
+        let validEmail = isValidInput(emailInput, email, emailRegex, emailInvalid);
+        let validpassword = isValidInput(passwordInput, password, passwordRegex, passwordInvalid);
+
+        console.log(validFirstName);
+        console.log(validLastName);
+        console.log(validEmail);
+        console.log(validpassword);
+
+        if (validFirstName && validLastName && validEmail && validpassword) {
+            const verifPassword = document.getElementById('verifPassword');
+            if (password !== checkPassword) {
+                verifPassword.nextElementSibling.innerHTML = '⛔ Mots de passe différents, réessayez.';
+                verifPassword.nextElementSibling.classList.remove('text-success');
+                verifPassword.nextElementSibling.classList.add('text-danger');
+                verifPassword.style.borderColor = 'red';
+            }
+            else {
+                let userData = {
+                    first_name: firstName,
+                    last_name: lastName,
+                    email: email,
+                    password: password
+                }
                 
-            </div>
-            )}
-            
-        </>
-    )
+                console.log(userData);
+
+                let fetchData = {
+                    method: 'POST',
+                    body: JSON.stringify(userData),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+
+                console.log(fetchData);
+
+                fetch('http://localhost:5500/api/auth/signup', fetchData)
+                    .then(response => response.json())
+                    .then(() => {
+                        setFormSubmit(true)
+                    })
+                        .catch((err) => {
+                            console.log(err);
+                            e.preventDefault();
+                        });
+                }
+        }
+    }
+
+
+
+    return (
+            <>
+            { formSumbit? (
+                    <>
+                        <div>Enregistrement réussi, vous pouvez maintenant vous connecter au réseau social</div>
+                        <Login />
+                    </>
+                ) : (
+                    <div>
+                    <Header/>
+                    <div className="d-flex justify-content-center">
+                        <div className="card p-3 m-3 col-md-8 d-flex align-items-center">
+                            <div className="mt-3 mb-4 h4">Inscription au réseau social de Groupomania</div>
+                            <form action='' onSubmit={ handleSubmit } method='POST' className="col-md-10 col-lg-7">
+                                <div className="form-group">
+                                    <label htmlFor='firstName' className="form-label">Prénom : </label>
+                                    <input type='text' name='firstName' id='firstName' className="form-control" onChange={(e) => setFirstName(e.target.value)} value= { firstName } /> 
+                                    <small className="small "></small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor='lastName' className="form-label">Nom : </label>
+                                    <input type='text' name='lastName' id='lastName' className="form-control" onChange={(e) => setLastName(e.target.value)} value= { lastName }  />
+                                    <small className="small"></small> 
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor='email' className="form-label">Adresse e-mail : </label>
+                                    <input type='text' name='email' id='email' className="form-control" onChange={(e) => setEmail(e.target.value)} value= { email } /> 
+                                    <small className="small"></small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor='password' className="form-label"> Mot de passe : </label>
+                                    <input type="password" name='password' id='password' className="form-control" onChange={(e) => setPassword(e.target.value)} value= { password } /> 
+                                    <small className="small"></small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor='verifPassword' className="form-label"> Vérification du mot de passe : </label>
+                                    <input type="password" name='verifPassword' id='verifPassword' className="form-control" onChange={(e) => setCheckPassword(e.target.value)} value= { checkPassword } /> 
+                                    <small className="small"></small>
+                                </div>
+                                <div >
+                                    <button type='submit' className="btn mt-3 rounded border">Inscription </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    
+                </div>
+                )}
+                
+            </>
+        )
 };
 
 export default Signup;
